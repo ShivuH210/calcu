3a2f16d1754933aa924d983d6283fdf72cced1a8
01fe22bca210 <01fe22bca210@kletech.ac.in>
2024-03-18 14:13:05 +0530

    addition

---
+++ calculator.java
@@ -0,0 +1,11 @@
+class calculator{
+    public int addition(int a,int b){
+        return (a+b);
+    }
+    public static void main(String[] args) {
+        calculator o=new calculator();
+        System.out.println(o.addition(0, 0));
+        
+    }
+    
+}
